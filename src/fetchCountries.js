3209c5b0e7dd1c3c155f3e4ebc505b089c129c42
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-09-20 18:11:58 +0300

    update task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,31 @@
 import Notiflix from 'notiflix';
-function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com';
-  return fetch(`${BASE_URL}/v2/name/${name}?fields=name,capital,population,flags,languages
-`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error();
-      }
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+
+export function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v2/name';
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error();
+  });
 }
-export { fetchCountries };
+
+// function fetchCountries(name) {
+//   const BASE_URL = 'https://restcountries.com';
+//   return fetch(`${BASE_URL}/v2/name/${name}?fields=name,capital,population,flags,languages
+// `)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         throw new Error();
+//       }
+//     })
+//     .catch(error => {
+// Notiflix.Notify.failure('Oops, there is no country with that name');
+//     });
+// }
+// export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
   ulEl: document.querySelector('.country-list'),
-  divEl: document.querySelector('.country-info'),
+  divEL: document.querySelector('.country-info'),
 };
 
 refs.input.addEventListener(
@@ -16,58 +16,126 @@ refs.input.addEventListener(
 );
 
 function onInputEventUser(event) {
-  clearPage();
   const value = event.target.value.trim();
   if (!value) {
     return;
   }
-  fetchCountries(value).then((data = []) => {
-    // console.log(data[0].languages[0]);
-    if (data.length > 10) {
-      Notiflix.Notify.info(
-        'Too many matches found. Please enter a more specific name.'
-      );
-      return;
-    }
-    if (data.length === 2 && data.length <= 10) {
-      renderMarkup(data);
-      return;
-    }
-    renderOneCountry(data);
-  });
+  fetchCountries(value)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        renderCountry(data);
+        return;
+      } else {
+        console.log(data);
+        renderOneCountry(data);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
 
-function renderMarkup(data) {
+function renderCountry(data) {
   const markup = data
     .map(({ flags, name }) => {
       return `<li class="item">
-      <img src="${flags.png}" alt="${name}" width="30" height="25"/>
+      <img src="${flags.svg}" alt="${name} width="30" height="30" ">
       <p>${name}</p>
     </li>`;
     })
     .join('');
+
   refs.ulEl.insertAdjacentHTML('beforeend', markup);
 }
 
 function renderOneCountry(data) {
   const markup = data
     .map(({ flags, name, capital, population, languages }) => {
-      const languagesArr = languages.map(value => value.name);
-      return `<li> 
-     <div class="container"> 
-      <img src="${flags.png}" alt="${name}" width="30" height="25"/>
+      const languagesArr = languages.map(item => item.name).join(', ');
+      return `<div>
       <h2>${name}</h2>
-   </div>
-    <p>Capital: ${capital}</p>
-    <p>Population: ${population}</p>
-    <p>Languages: ${languagesArr}</p>
-    </li>`;
+      <img src="${flags.svg}" alt="${name}">
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${languagesArr}</p>
+          </div>`;
     })
     .join('');
+  console.log(markup);
+
   refs.ulEl.insertAdjacentHTML('beforeend', markup);
 }
 
-function clearPage() {
-  refs.ulEl.innerHTML = '';
-  refs.divEl.innerHTML = '';
-}
+// const refs = {
+//   input: document.querySelector('#search-box'),
+//   ulEl: document.querySelector('.country-list'),
+//   divEl: document.querySelector('.country-info'),
+// };
+
+// refs.input.addEventListener(
+//   'input',
+//   debounce(onInputEventUser, DEBOUNCE_DELAY)
+// );
+
+// function onInputEventUser(event) {
+//   clearPage();
+//   const value = event.target.value.trim();
+//   if (!value) {
+//     return;
+//   }
+//   fetchCountries(value).then((data = []) => {
+//     // console.log(data[0].languages[0]);
+//     if (data.length > 10) {
+//       Notiflix.Notify.info(
+//         'Too many matches found. Please enter a more specific name.'
+//       );
+//       return;
+//     }
+//     if (data.length === 2 && data.length <= 10) {
+//       renderMarkup(data);
+//       return;
+//     }
+//     renderOneCountry(data);
+//   });
+// }
+
+// function renderMarkup(data) {
+//   const markup = data
+//     .map(({ flags, name }) => {
+//       return `<li class="item">
+//       <img src="${flags.png}" alt="${name}" width="30" height="25"/>
+//       <p>${name}</p>
+//     </li>`;
+//     })
+//     .join('');
+//   refs.ulEl.insertAdjacentHTML('beforeend', markup);
+// }
+
+// function renderOneCountry(data) {
+//   const markup = data
+//     .map(({ flags, name, capital, population, languages }) => {
+//       const languagesArr = languages.map(value => value.name);
+//       return `<li>
+//      <div class="container">
+//       <img src="${flags.png}" alt="${name}" width="30" height="25"/>
+//       <h2>${name}</h2>
+//    </div>
+//     <p>Capital: ${capital}</p>
+//     <p>Population: ${population}</p>
+//     <p>Languages: ${languagesArr}</p>
+//     </li>`;
+//     })
+//     .join('');
+//   refs.ulEl.insertAdjacentHTML('beforeend', markup);
+// }
+
+// function clearPage() {
+//   refs.ulEl.innerHTML = '';
+//   refs.divEl.innerHTML = '';
+// }
